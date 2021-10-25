abe782bc719eb9a23b2aae331d986f9497debaa9
Paweł <pawelos141@gmail.com>
2021-10-26 01:12:57 +0200

    Add files via upload
    Poprawne obliczanie spalania.

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,6 +1,7 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -13,3 +14,15 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
+
+void MainWindow::on_Oblicz_clicked()
+{
+    long double b = ui->dystanss->value();
+    long double a = ui->paliwoo->value();
+    if(b!=0){
+    ui->wynikk->setValue((a/b)*100);
+}
+    else {
+        ui->wynikk->setValue(0);
+}
+}
